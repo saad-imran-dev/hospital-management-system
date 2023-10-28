ef85d5307b0b34a6ab42c428826bee72f106548d
Muhmmad Saad <saad.imran.vohra@gmail.com>
2023-10-28 07:25:02 -0700

    App Setup fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { ThemeProvider } from '@mui/material'
-import theme from './constants'
+import { theme } from './constants'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
