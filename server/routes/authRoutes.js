52be2816a37eb84130b0997dabb435f482cd2e07
Ammar Siddiqui <ammarsiddiqui247@gmail.com>
2023-12-02 00:45:36 +0500

    Added swagger documentation at route '/api-docs'. Implemented all required functionalities as mentioned

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,236 @@
+const express = require('express')
+
+const authRouter = express.Router()
+const db = require('../database/database')
+
+
+/**
+ * @swagger
+ * tags:
+ *  name : auth
+ *  description : API for logging in
+ */
+
+
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      auth:
+ *          type : object
+ *          required:
+ *              - email
+ *              - password
+ *          properties:
+ *              email:
+ *                  type : string
+ *              password :
+ *                  type : string
+ */
+
+
+/**
+ * @swagger
+ * /auth/admin:
+ *  post:
+ *      summary : Login for admin
+ *      tags : [auth]
+ *      requestBody : 
+ *          required : true
+ *          content :
+ *              application/json:
+ *                  schema:
+ *                      $ref : '#/components/schemas/auth'
+ *          responses:
+ *              200:
+ *                  description : Logged in successfully
+ *              401:
+ *                  description : Invalid credentials
+ */
+
+authRouter.post('/admin' , async (req,res) => {
+    const email = req.body.email
+    const password = req.body.password
+
+    const query = "SELECT * FROM ADMIN_TABLE WHERE EMAIL = $1::TEXT AND PASS = $2::TEXT"
+    const params = [email , password]
+    try {
+        const results = await db.dbQuery(query , params)
+        if (results){
+            if(results.length > 0){
+                let accessToken = jwt.sign(
+                    {data : password},
+                    'adminSecret',
+                    )
+                req.session.authorization = {
+                    accessToken,
+                    email
+                }
+                res.status(200).send("Successfully Logged in");
+            }
+            else{
+                res.status(401).send("Invalid credentials. Kindly retry");
+            }
+        }
+    }
+    catch(error){
+        console.log(error)
+        res.status(500).send('Server side error')
+    }
+})
+/**
+ * @swagger
+ * /auth/opd:
+ *  post:
+ *      summary : Login for admin
+ *      tags : [auth]
+ *      requestBody : 
+ *          required : true
+ *          content :
+ *              application/json:
+ *                  schema:
+ *                      $ref : '#/components/schemas/auth'
+ *          responses:
+ *              200:
+ *                  description : Logged in successfully
+ *              401:
+ *                  description : Invalid credentials
+ */
+
+authRouter.post('/opd' , async (req,res) => {
+    const email = req.body.email
+    const password = req.body.password
+
+    const query = "SELECT * FROM OPD WHERE EMAIL = $1::TEXT AND PASS = $2::TEXT";
+    const params = [email,password]
+    try {
+        const results = await db.dbQuery(query , params)
+        if (results){
+            if(results.length > 0){
+                let accessToken = jwt.sign(
+                    {data : password},
+                    'opdSecret',
+                    )
+                req.session.authorization = {
+                    accessToken,
+                    email
+                }
+                res.status(200).send("Successfully Logged in");
+            }
+            else{
+                res.status(401).send("Invalid credentials. Kindly retry");
+            }
+        }
+    }
+    catch(error){
+        console.log(error)
+        res.status(500).send('Server side error')
+    }
+})
+/**
+ * @swagger
+ * /auth/doctor:
+ *  post:
+ *      summary : Login for admin
+ *      tags : [auth]
+ *      requestBody : 
+ *          required : true
+ *          content :
+ *              application/json:
+ *                  schema:
+ *                      $ref : '#/components/schemas/auth'
+ *          responses:
+ *              200:
+ *                  description : Logged in successfully
+ *              401:
+ *                  description : Invalid credentials
+ */
+
+authRouter.post('/doctor' , async (req,res) => {
+    const email = req.body.email
+    const password = req.body.password
+
+    const query = "SELECT * FROM DOCTOR WHERE EMAIL = $1::TEXT AND PASS = $2::TEXT";
+    const params = [email,password]
+    try {
+        const results = await db.dbQuery(query , params)
+        if (results){
+            if(results.length > 0){
+                let accessToken = jwt.sign(
+                    {data : password},
+                    'doctorSecret',
+                    )
+                req.session.authorization = {
+                    accessToken,
+                    email
+                }
+                res.status(200).send("Successfully Logged in");
+            }
+            else{
+                res.status(401).send("Invalid credentials. Kindly retry");
+            }
+        }
+    }
+    catch(error){
+        console.log(error)
+        res.status(500).send('Server side error')
+    }
+    
+})
+
+//login patient
+/**
+ * @swagger
+ * /auth/patient:
+ *  post:
+ *      summary : Login for admin
+ *      tags : [auth]
+ *      requestBody : 
+ *          required : true
+ *          content :
+ *              application/json:
+ *                  schema:
+ *                      $ref : '#/components/schemas/auth'
+ *          responses:
+ *              200:
+ *                  description : Logged in successfully
+ *              401:
+ *                  description : Invalid credentials
+ */
+
+authRouter.post('/patient' , async (req,res) => {
+    const email = req.body.email
+    const password = req.body.password
+
+    const query = "SELECT * FROM PATIENT WHERE EMAIL = $1::TEXT AND PASS = $2::TEXT";
+    const params = [email,password]
+    try {
+        const results = await db.dbQuery(query , params)
+        if (results){
+            if(results.length > 0){
+                let accessToken = jwt.sign(
+                    {data : password},
+                    'patientSecret',{expiresIn:60*60}
+                    )
+                req.session.authorization = {
+                    accessToken,
+                    email
+                }
+                res.status(200).send("Successfully Logged in");
+            }
+            else{
+                res.status(404).send("Invalid credentials. Kindly retry");
+            }
+        }
+    }
+    catch(error){
+        console.log(error)
+        res.status(500).send('Server side error')
+    }
+})
+
+
+
+
+
+module.exports = {authRouter}
--- server/routes/doctorRouter.js
+++ server/routes/doctorRouter.js
@@ -4,10 +4,67 @@ const doctorRouter = express.Router()
 const db = require('../database/database')
 
 
-doctorRouter.use(middleware.doctorMiddleware)
+// doctorRouter.use(middleware.doctorMiddleware)
+
+
+/**
+ * @swagger
+ * tags:
+ *  name : doctor
+ *  description : API for doctors
+ */
+
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      Doctor:
+ *          type: object
+ *          properties:
+ *              first_name:
+ *                  type : string
+ *              last_name:
+ *                  type : string
+ *              dept_name:
+ *                  type : string
+ *              email:
+ *                  type : string
+ *              password:
+ *                  type : string
+ */
+
+
+
 //get data of all doctors without logging in
+/**
+ * @swagger
+ * /doctor:
+ *      get:
+ *          summary : Get the list of all doctors
+ *          tags : [doctor]
+ *          responses:
+ *              200:
+ *                  description: List of all doctors
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type : object
+ *                              properties:
+ *                                  first_name:
+ *                                      type : string
+ *                                  last_name:
+ *                                      type : string
+ *                                  department_name:
+ *                                      type : string
+ *                              example:
+ *                                  first_name : Ammar
+ *                                  last_name : Siddiqui
+ *                                  department_name : Neurology
+ * 
+ * 
+ */
 doctorRouter.get('/' , async (_,res) => {
-    const query = "SELECT * FROM DOCTOR";
+    const query = "SELECT D.first_name,D.last_name,DEPT.DEPT_NAME FROM DOCTOR D, DEPARTMENT DEPT WHERE D.DEPT_ID=DEPT.DEPT_ID";
     const results = await db.dbQuery(query , [])
 
     if(results){
@@ -18,6 +75,42 @@ doctorRouter.get('/' , async (_,res) => {
     }
 })
 
+
+/**
+ * @swagger
+ * /doctor/prescription:
+ *  post:
+ *      summary : Give patient the prescription
+ *      tags : [doctor]
+ *      requestBody:
+ *          required : true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          patient:
+ *                              type: object
+ *                              properties: 
+ *                                  first_name:
+ *                                      type : string
+ *                                  last_name:
+ *                                      type : string
+ *                              required:
+ *                                  - first_name
+ *                                  - last_name
+ *                          date:
+ *                              type : string
+ *                          prescription:
+ *                              type : string
+ *          responses:
+ *              '200' : 
+ *                  description : 'Added successfully'
+ *              '404' :
+ *                  description : 'Invalid data entered'
+ *              '402':
+ *                  description : 'Not logged in'
+ */
 //doctor post prescription of patient
 doctorRouter.post('/prescription' , async(req,res) => {
     /*
@@ -55,6 +148,32 @@ doctorRouter.post('/prescription' , async(req,res) => {
 })
 
 //doctor can see his/her patients
+/**
+ * @swagger
+ * /doctor/patients:
+ *  get:
+*       summary : Get all the patients of given doctor
+*       tags : [doctor]
+*       parameters:
+*           - name : first_name
+*             in : query
+*             description : First name of doctor
+*             required : true
+*             type : string
+*           - name : last_name
+*             in : query
+*             description : Last name of the doctor
+*             required : true
+*             type : true
+*       responses : 
+*           '200':
+*               description : Record retrieved successfully
+*           '404':
+*               description : Invalid details of doctor
+*           '403':
+*               description : Doctor not logged in
+ *
+ */
 doctorRouter.get('/patients' , async(req , res) => {
     const first_name = req.query.first_name
     const last_name = req.query.last_name
@@ -72,4 +191,91 @@ doctorRouter.get('/patients' , async(req , res) => {
     }
 }) 
 
+//doctor can see the previous prescription he has given to the patient using their cnic
+
+/**
+ * @swagger
+ * /doctor/patients/{cnic}:
+ *  get:
+ *      summary : Get prescriptions of a patient provided his/her cnic
+ *      tags : [doctor]
+ *      parameters :
+ *          - name : cnic
+ *            in : path
+ *            description : Cnic of the patient whose prescriptions are required
+ *            required : true
+ *            type : string
+ *      responses :
+ *          '200':
+ *              description : Success
+ *          '403':
+ *              description : Not logged in
+ *          '404':
+ *              description : No previos prescription found;
+ *        
+ */
+
+doctorRouter.get('/patients/:cnic' , async (req , res) => {
+    const cnic = req.params.cnic
+
+    const query = "SELECT A.PRESCRIPTION,A.APPOINTMENT_DATE,P.FIRST_NAME,P.LAST_NAME FROM APPOINTMENT A,PATIENT P WHERE P.CNIC = $1::TEXT AND P.PATIENT_ID = A.PATIENT_ID";
+    const params = [cnic]
+
+    const results = await db.dbQuery(query,params)
+    if(results.length > 0){
+        res.status(200).send(results)
+        return
+    }
+    else{
+        res.status(404).send({"message" : "Cannot find any previous appointment of given patient"})
+        return
+    }
+})
+
+
+/**
+ * @swagger
+ *  /doctor:
+ *  post:
+ *      summary : Register a doctor
+ *      tags : [doctor]
+ *      requestBody:
+ *          required : true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/Doctor'
+ *      responses:
+ *          200:
+ *              description : Registered Successfully
+ *          402:
+ *              description : Invalid details entered.
+ *          500:
+ *              description : internal server error.
+*/
+
+doctorRouter.post('/' , async(req , res) => {
+    const body = req.body
+    if (!body.first_name || !body.last_name || !body.department_name || !body.email || !body.password){
+        res.status(402).send('Invalid details entered. Kindly fill all fields')
+        return
+    }
+    let query = "SELECT DEPT_ID FROM DEPARTMENT WHERE DEPT_NAME = $1::TEXT";
+    let params = [body.department_name]
+    const dept_id = await db.dbQuery(query , params)
+    
+
+    query = "INSERT INTO DOCTOR (FIRST_NAME,LAST_NAME,DEPT_ID,EMAIL,PASS) VALUES ($1::TEXT,$2::TEXT,$3::NUMBER,$4::TEXT,$5::TEXT)"
+    params = [body.first_name,body.last_name,dept_id[0].dept_id,body.email,body.password]
+
+    const results = await db.dbQuery(query , params)
+
+    if(results !== null){
+        res.status(200).send('Doctor added successfully')
+    }
+    else{
+        res.status(500).send('Server Error')
+    }
+
+})
 module.exports = {doctorRouter}
